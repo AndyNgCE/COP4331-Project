245cd198864f9d5c34778301106b193593139623
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-02-14 20:24:48 -0500

    PowerUp.ccp
    Currently not ready for use but, essentially will create static mesh for actor and display text on collision with players.

---
+++ KingOfBombs/Source/KingOfBombs/Public/PowerUp.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PowerUp.generated.h"
+
+UCLASS()
+class KINGOFBOMBS_API APowerUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APowerUp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Sets static mesh for power-ups
+	UPROPERTY(VisibleAnywhere)
+		UStaticMeshComponent* PowerMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		class UBoxComponent* Collisionbox;
+
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
