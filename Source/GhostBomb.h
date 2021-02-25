1e2b6af2a9341f343b658af978c6577a6480f571
Gabriel Barcenas <gabebarcenas@knights.ucf.edu>
2021-02-24 21:35:31 -0500

    Added GhostBomb.cpp and linked blueprint to it

---
+++ Source/GhostBomb.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bomb.h"
+#include "GhostBomb.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KINGOFBOMBS_API AGhostBomb : public ABomb
+{
+	GENERATED_BODY()
+	
+};
