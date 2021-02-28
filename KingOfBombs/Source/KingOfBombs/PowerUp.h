d7ab917fb832b2d3af7148333025d70f8408f39e
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-02-27 23:30:58 -0500

    Update PowerUp.h

--- KingOfBombs/Source/KingOfBombs/PowerUp.h
+++ KingOfBombs/Source/KingOfBombs/PowerUp.h
@@ -46,7 +46,7 @@ public:
 	float Radius = 53.0f;
 	bool Equipped = false;
 	// Duration of effect - debatable if effects should be permanant or not
-	int Duration;
+	float Duration;
 
 	UFUNCTION()
 		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
