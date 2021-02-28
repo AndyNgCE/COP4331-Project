7a0296a431fd6f58fb64f03b161eeffc592ad6da
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-02-27 20:04:32 -0500

    Update Hazzard.cpp

--- KingOfBombs/Source/KingOfBombs/Hazzard.cpp
+++ KingOfBombs/Source/KingOfBombs/Hazzard.cpp
@@ -40,7 +40,7 @@ void AHazzard::AffectPlayer(class AActor* OtherActor)
 
 void AHazzard::OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && (OtherActor != this) && OtherComp)
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
 	{
 		AffectPlayer(OtherActor);
 	}
