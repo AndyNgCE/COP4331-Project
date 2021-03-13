b295a843a84afd0d83e3e4fd5c999d37bb322538
AndyNgCE <71106921+AndyNgCE@users.noreply.github.com>
2021-03-12 20:00:49 -0500

    Revert "Update Bomb.cpp and bombHitbox.cpp"
    This reverts commit f7d0e9e7224879fbd72408f9061f70576d4727b9.

--- KingOfBombs/Source/KingOfBombs/bombHitBox.cpp
+++ KingOfBombs/Source/KingOfBombs/bombHitBox.cpp
@@ -8,7 +8,7 @@ AbombHitBox::AbombHitBox()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	BlastRadius = 50 + (50 * 1);
+
 }
 
 // Called when the game starts or when spawned
@@ -24,5 +24,4 @@ void AbombHitBox::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
 
